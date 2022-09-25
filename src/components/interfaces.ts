50f63f79c4722e6580eee62b4bbccb651437895a
DmitrySaf <dmitry.study@yandex.ru>
2022-09-25 22:10:05 +0300

    added interfaces

---
+++ src/components/interfaces.ts
@@ -0,0 +1,18 @@
+interface IGood {
+  id?: number,
+  date?: string,
+  name?: string,
+  quantity?: number,
+  distance?: number
+}
+
+interface ISearchFilter {
+  searchValue?: string,
+  column?: string,
+  clause?: string
+}
+
+export type {
+  IGood,
+  ISearchFilter
+}
